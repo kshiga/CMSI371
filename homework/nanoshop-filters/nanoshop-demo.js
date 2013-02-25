bb45cd28b753e3c4b9e11148a756a91e9b086aae
KSH <ksh@ubuntu.(none)>
2013-02-25 14:43:41 -0800

    separated filters from demo.js, added to nanoshop.  For some reason, image will not load

---
+++ homework/nanoshop-filters/nanoshop-demo.js
@@ -0,0 +1,78 @@
+/*
+ * This demo script uses the Nanoshop module to apply a simple
+ * filter on a canvas drawing.
+ */
+(function () {
+    var canvas = $("#picture")[0],
+        renderingContext = canvas.getContext("2d"),
+        gradient;
+        sampleIMG = new Image();
+        sampleIMG.src = 'sample.jpg';
+        renderingContext.drawImage(sampleIMG, 0, 0);
+/*
+    // Adapted from original code by Tyler Nichols.
+    gradient = renderingContext.createRadialGradient(120, 120, 15, 120, 120, 75);
+    gradient.addColorStop(0, "rgb(255, 102, 102)");
+    gradient.addColorStop(1, "red");
+
+    // Draw the sphere with a radial gradient.
+    renderingContext.beginPath();
+    renderingContext.fillStyle = gradient;
+    renderingContext.arc(150, 150, 75, 0, 2 * Math.PI, true);
+    renderingContext.shadowColor = "gray";
+    renderingContext.shadowBlur = 20;
+    renderingContext.shadowOffsetX = 10;
+    renderingContext.shadowOffsetY = 15;
+    renderingContext.fill();
+    renderingContext.closePath();
+
+    // Draw the top of the cube.
+    renderingContext.beginPath();
+    renderingContext.fillStyle = "rgb(140, 140, 140)";
+    renderingContext.moveTo(300, 300);
+    renderingContext.lineTo(335, 265);
+    renderingContext.lineTo(435, 265);
+    renderingContext.lineTo(400, 300);
+    renderingContext.lineTo(300, 300);
+    renderingContext.fill();
+    renderingContext.closePath();
+
+    // Draw the face of the cube.
+    renderingContext.fillStyle = "rgb(110, 110, 110)";
+    renderingContext.fillRect(300, 300, 100, 100);
+
+    // Draw the right side of the cube.
+    renderingContext.beginPath();
+    renderingContext.fillStyle = "rgb(79, 79, 79)";
+    renderingContext.moveTo(435, 265);
+    renderingContext.lineTo(435, 355);
+    renderingContext.lineTo(400, 400);
+    renderingContext.lineTo(400, 300);
+    renderingContext.lineTo(435, 265);
+    renderingContext.fill();
+    renderingContext.closePath();
+    // (end of adapted code by Tyler Nichols)
+*/
+    // Set a little event handler to apply the filter.
+    $("#darken-filter-button").click(function () {
+        renderingContext.putImageData(
+            Nanoshop.applyFilter(
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                Nanoshop.darken
+            ),
+            0, 0
+        );
+
+    });
+
+    $("#bAndw-filter-button").click(function () {
+        renderingContext.putImageData(
+            Nanoshop.applyFilter(
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                Nanoshop.bAndw
+            ),
+            0, 0
+        );
+
+    });
+}());
